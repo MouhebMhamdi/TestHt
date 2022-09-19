63f608f81af6697994edff6607c85a11bda50602
Dhia <mohameddhia.mosbahi@esprit.tn>
2022-09-19 23:53:19 +0100

    Prepare all ressources and install tailwindcss and config it and design the tamplate

---
+++ src/App.svelte
@@ -0,0 +1,22 @@
+<script>
+  import NavBar from "./Components/Shared/NavBar.svelte";
+  import Auth from "./Routes/Auth.svelte";
+  import Home from "./Routes/Home.svelte";
+  import NotFound from "./Routes/NotFound.svelte";
+  import Router from 'svelte-spa-router'
+
+  let routes={
+    '/': Home,
+    '/auth': Auth,
+    '*': NotFound
+  }
+
+
+  
+</script>
+
+<NavBar />
+<main>
+  <Router {routes} />
+
+</main>
---
+++ tailwind.config.cjs
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html', './src/**/*.{svelte,js,ts}'],
+  theme: {
+    
+    container: {
+      center: true,
+      padding: '2rem',
+    },
+    extend: {},
+  },
+  plugins: [],
+}
